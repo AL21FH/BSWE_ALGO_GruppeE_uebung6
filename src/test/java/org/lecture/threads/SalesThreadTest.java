a4dc4e90e55000f246072167de33f8b4aa451360
Khaled <2210859032@fh-burgenland.at>
2024-01-06 01:02:19 +0100

    Test Class for SalesThread Updated to handle interruptions more effectively. added Documentation.

--- src/test/java/org/lecture/threads/SalesThreadTest.java
+++ src/test/java/org/lecture/threads/SalesThreadTest.java
@@ -1,14 +1,29 @@
 package org.lecture.threads;
 
+import org.junit.Test;
 import org.lecture.interfaces.LagerOperation;
 import org.lecture.storage.WarehouseManagement;
 
-
+/**
+ * Unit tests for the SalesThread class.
+ */
 public class SalesThreadTest {
 
-    public static void main(String[] args) {
+    /**
+     * Tests the behavior of the SalesThread class by starting the thread,
+     * letting it run for a specified duration, then interrupting and waiting for it to finish.
+     *
+     * <p>
+     * This test checks the interruption mechanism and ensures the thread behaves as expected.
+     * </p>
+     */
+    @Test
+    public void testSalesThread() {
+        // Arrange
         LagerOperation lagerOperation = new WarehouseManagement();
         SalesThread salesThread = new SalesThread(lagerOperation);
+
+        // Act
         salesThread.start();
 
         try {
