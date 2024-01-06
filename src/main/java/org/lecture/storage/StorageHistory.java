b283cf486cf1687edbcc5118aa53c2af061177c3
Khaled <2210859032@fh-burgenland.at>
2024-01-06 01:07:59 +0100

    added documentation.

--- src/main/java/org/lecture/storage/StorageHistory.java
+++ src/main/java/org/lecture/storage/StorageHistory.java
@@ -6,11 +6,22 @@ import lombok.Setter;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The {@code StorageHistory} class represents the history of storage operations.
+ * It maintains a list of entries to track the historical changes in the storage.
+ */
 @Getter
 @Setter
 public class StorageHistory {
+
+    /**
+     * The list to store historical entries.
+     */
     private List<String> history = new ArrayList<>();
 
+    /**
+     * Adds a new entry to the storage history.
+     */
     public void addEntry(String entry) {
         history.add(entry);
     }
